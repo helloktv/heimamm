0b77e1d30f25dc09a8b24e0065e40db98a3d9766
szj <helloktv@126.com>
2020-02-07 11:02:34 +0800

    ceshi

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
+//导入和注册element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
 import App from './App.vue'
-
+import router from './router/index'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+  //挂载在vue实例上
+  router
+})
+//挂载到id为app的div上面
+.$mount('#app')
