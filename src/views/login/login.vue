3de6c8d8cb0b07358c0cd3820ece2175f5dee113
szj <helloktv@126.com>
2020-02-08 14:09:06 +0800

    登录页面完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -9,41 +9,45 @@
         <div class="line"></div>
         <div class="userLogin">用户登录</div>
       </div>
-      <div class="inprint">
-        <el-input
-          class="item"
-          placeholder="请输入手机号"
-          prefix-icon="el-icon-user"
-          v-model="phoneNumber"
-        ></el-input>
-        <el-input
-          class="item"
-          placeholder="请输入密码"
-          show-password
-          prefix-icon="el-icon-goods"
-          v-model="passWord"
-        ></el-input>
-        <el-row class="total-row">
-          <el-col :span="17">
-            <el-input
-              class="lastitem"
-              placeholder="请输入验证码"
-              prefix-icon="el-icon-key"
-              v-model="securityCode"
-            ></el-input>
-          </el-col>
-          <el-col :span="7">
-            <div class="number"></div>
-          </el-col>
-        </el-row>
-      </div>
-      <div class="protocol">
-        <el-checkbox v-model="checked"></el-checkbox>我已阅读和同意
-        <el-link type="primary">隐私协议</el-link>和
-        <el-link type="primary">隐私条款</el-link>
-      </div>
-      <el-button type="primary" class="login buttons">登录</el-button>
-      <el-button type="primary" class="login buttons">注册</el-button>
+      <el-form ref="form" :model="loginForm" label-width="43px">
+        <el-form-item>
+          <el-input placeholder="请输入手机号" prefix-icon="el-icon-user" v-model="loginForm.phoneNumber"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            placeholder="请输入密码"
+            show-password
+            prefix-icon="el-icon-goods"
+            v-model="loginForm.passWord"
+          ></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-row class="total-row">
+            <el-col :span="17">
+              <el-input
+                class="lastitem"
+                placeholder="请输入验证码"
+                prefix-icon="el-icon-key"
+                v-model="loginForm.securityCode"
+              ></el-input>
+            </el-col>
+            <el-col :span="7">
+              <img class="number" src="../../assets/yanzhengma.png" alt />
+            </el-col>
+          </el-row>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked">
+            我已阅读和同意
+            <el-link type="primary">隐私协议</el-link>和
+            <el-link type="primary">隐私条款</el-link>
+          </el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="login buttons">登录</el-button>
+          <el-button type="primary" class="login buttons">注册</el-button>
+        </el-form-item>
+      </el-form>
     </div>
     <img src="../../assets/login_banner_ele.png" alt />
   </div>
@@ -54,10 +58,12 @@ export default {
   name: "login",
   data() {
     return {
-      phoneNumber: "",
-      passWord: "",
-      securityCode: "",
-      checked: false
+      loginForm: {
+        phoneNumber: "",
+        passWord: "",
+        securityCode: "",
+        checked: false
+      }
     };
   }
 };
@@ -79,9 +85,12 @@ export default {
     width: 478px;
     height: 550px;
     background: rgba(245, 245, 245, 1);
+    padding-right: 41px;
+    box-sizing: border-box;
     .top {
       margin-left: 48px;
       margin-top: 50px;
+      margin-bottom: 31px;
       display: flex;
       align-items: center;
       .heima {
@@ -105,44 +114,27 @@ export default {
         color: rgba(12, 12, 12, 1);
       }
     }
-    .inprint {
-      .item {
-        height: 43px;
-        margin-top: 25px;
-        margin-left: 43px;
-        width: 394px;
-      }
-      .total-row {
-        width: 394px;
-        height: 44px;
-        margin-top: 25px;
-        margin-left: 43px;
-        .number {
-          height: 42px;
-        }
+    .number {
+      width: 100%;
+    }
+    .el-checkbox {
+      display: flex;
+      align-items: center;
+      .el-checkbox__label {
+        display: flex;
       }
     }
-    .protocol {
-      margin-left: 44px;
-      margin-top: 32px;
-      height: 16px;
-      font-size: 14px;
-      font-family: Microsoft YaHei;
-      font-weight: 400;
-      color: rgba(153, 153, 153, 1);
-      line-height: 16px;
-    }
+
     .buttons {
       width: 394px;
       height: 40px;
       background: rgba(20, 147, 250, 1);
       border-radius: 4px;
-      margin-top:27px;
-      margin-left: 42px;
+      margin-top: 27px;
     }
-    .buttons+.buttons {
-    margin-left: 42px;
-}
+    .buttons + .buttons {
+      margin-left: 0px;
+    }
   }
 }
 </style>
